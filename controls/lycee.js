3de0fc09327a8f2cb20880579cd06ab4d5050752
spelhate <seb.pelhate@gmail.com>
2016-05-20 09:54:07 +0200

    Transform customControls js in closure functions

--- controls/lycee.js
+++ controls/lycee.js
@@ -1,15 +1,26 @@
-var idlayer = 'lycee';
-mviewer.customControls[idlayer] = {};
-var layerDefinition = mviewer.getLayer(idlayer);
-console.log(layerDefinition);
-var source = layerDefinition.layer.getSource();
+mviewer.customControls.lycee = (function() {
+    /*
+     * Private
+     */
+    var _idlayer = 'lycee';
+    var _layerDefinition = mviewer.getLayer(_idlayer);
+    var _source = _layerDefinition.layer.getSource();
+    
+    return {
+        /*
+         * Public
+         */
 
-mviewer.customControls[idlayer].setStyle = function  (style) {
-    source.getParams()['STYLES'] = style;
-    layerDefinition.style = style;
-    layerDefinition.fields = ["nom", "libelle"];
-    layerDefinition.aliases = ["Truc", "Muche"];
-    source.changed();
-    var legendUrl = mviewer.setLayerLegend(layerDefinition);
-    $("#legend-" + idlayer).attr("src", legendUrl);    
-};
+        setStyle:  function  (select) {
+            var style = select.value;
+            var label = $(select).find('option:selected').data('label');            
+            _source.getParams()['STYLES'] = style;
+            _layerDefinition.style = style;            
+            _source.changed();
+            var legendUrl = mviewer.setLayerLegend(_layerDefinition);
+            $("#legend-" + _idlayer).attr("src", legendUrl);    
+            $("[data-layer="+_idlayer+"] h3 td").first().text(_layerDefinition.name + " - " + label);
+        }
+     }; // fin return	
+
+}());
